de7248e911ffc7f9f0a01d04613ba188af2c0eaa
AlexCoventry2023 <alexcoventry2015@gmail.com>
2024-06-05 10:57:02 +0100

    AC Update after Single-Item

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,38 +1,83 @@
-import React, { useState, useEffect } from 'react';
-// import { SaucesList } from './SaucesList';
+import React, { useEffect, useState } from "react";
 
-// import and prepend the api url to any fetch calls
-import apiURL from '../api';
+import apiURL from "../api";
 
 export const App = () => {
 	const [items, setItems] = useState([]);
+	const [currentItem, setCurrentItem] = useState(null);
+
+	async function deleteItem(id) {
+		const response = await fetch(`${apiURL}/items/${id}`, {
+			method: "DELETE",
+		});
+
+		if (response.ok) {
+			const filteredItems = items.filter(item => {
+				if (item.id === id) {
+					return false;
+				} else {
+					return true;
+				}
+			});
+
+			setItems(filteredItems);
+
+			setCurrentItem(null);
+		}
+	}
+
+	function confirmDelete(id) {
+		const confirmed = window.confirm("Are you sure you want to delete this item?");
+
+		if (confirmed) {
+			deleteItem(id);
+		}
+	}
 
 	useEffect(() => {
-		
-		async function fetchItems(){
+		async function fetchItems() {
 			try {
-				const response = await fetch(`${apiURL}/items`);//JSON string
-				const itemsData = await response.json();// turns JSON string into JS object
+				const response = await fetch(`${apiURL}/items`);
+				const itemsData = await response.json();
 				setItems(itemsData);
 			} catch (err) {
-				console.log("Oh no an error! ", err)
+				console.log("Oh no an error!", err);
 			}
 		}
-	
+
 		fetchItems();
 	}, []);
+
+	if (!currentItem) {
+		return (
+			<main>
+				<h1>Inventory App</h1>
+				<ul>
+					{items.map(item => (
+						<li key={item.id}>
+							<h2>
+								<button onClick={() => setCurrentItem(item)}>{item.name}</button>
+							</h2>
+							<img src={item.image} alt="" />
+						</li>
+					))}
+				</ul>
+			</main>
+		);
+	}
+
 	return (
-		<main>	
-      <h1>Inventory App</h1>
-	  <ul>
-		{items.map(item =>(
-			<li key={item.id}>
-				<h2>{item.name}</h2>
-				<img src={item.image} alt =""/>
-			</li>
-		))}
-	  </ul>
-			{/*<SaucesList sauces={sauces}> */}
-	</main>
+		<main>
+			<h1>{currentItem.name}</h1>
+			<p>£{currentItem.price.toFixed(2)}</p>
+			<p>{currentItem.description}</p>
+			<img src={currentItem.image} alt="" />
+			<p>
+				<button onClick={() => setCurrentItem(null)}>All Items</button>
+			</p>
+			<p>
+				<button onClick={() => confirmDelete(currentItem.id)}>Delete Item</button>
+			</p>
+		</main>
 	);
 };
